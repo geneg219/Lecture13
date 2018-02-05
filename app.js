261190e1ecfab354cd5daf04db1e444b3e43b20c
Gene <gene@coursera-blah.com>
2018-02-04 22:51:42 -0800

    full solution

--- app.js
+++ app.js
@@ -4,35 +4,47 @@
 
 angular
     .module('MsgApp', [])
-    .controller('MsgController', MsgController);
+    .controller('MsgController', MsgController)
+    .filter('truth', TruthFilter);
 
     MsgController.$inject = ['$scope', '$filter'];
 
     function MsgController($scope, $filter) {
 
-      $scope.name = "Yaakov";
-      $scope.stateOfBeing = "hungry";
-      $scope.cookieCost = .45;
-      $scope.btnText = "Feed Yaakov";
+        $scope.name = "Yaakov";
+        $scope.stateOfBeing = "hungry";
+        $scope.cookieCost = .45;
+        $scope.btnText = "Feed Yaakov";
 
-      $scope.sayMessage = function () {
-          var msg = "Yaakov likes to eat healthy snacks at night!";
-          var output = $filter('uppercase')(msg);
-          return output;
-      };
+        $scope.sayMessage = function () {
+            var msg = "Yaakov likes to eat healthy snacks at night!";
+            var output = $filter('uppercase')(msg);
+            return output;
+        };
 
-      $scope.feedYaakov = function () {
-          if ($scope.stateOfBeing == "fed") {
-              $scope.stateOfBeing = "hungry";
-              $scope.btnText = "Feed Yaakov";
-          }
-          else {
-              $scope.stateOfBeing = "fed";
-              $scope.btnText = "Unfeed Yaakov";
-          }
-      };
+        $scope.sayTruthMessage = function () {
+            var msg = "Yaakov likes to eat healthy snacks at night!";
+            return msg;
+        };
 
-      
-}
+        $scope.feedYaakov = function () {
+            if ($scope.stateOfBeing == "fed") {
+                $scope.stateOfBeing = "hungry";
+                $scope.btnText = "Feed Yaakov";
+            }
+            else {
+                $scope.stateOfBeing = "fed";
+                $scope.btnText = "Unfeed Yaakov";
+            }
+        };
+    }
+
+    function TruthFilter() {
+        return function(input, target, replace) {
+            input = input || "";
+            input = input.replace(target, replace);
+            return input;
+        }
+    }
 
 })();
